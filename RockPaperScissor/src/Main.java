6c02536b771d38cb0a975d9dccd4e5c76c807dc2
Rajesh Murlidhar Shedolkar <rajeshshedolkar12@gmail.com>
2024-05-07 22:59:59 +0530

    update

---
+++ RockPaperScissor/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        // start the game
+        String p1, p2, p3;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Add Player1: ");
+        p1 = sc.next();
+        System.out.println("Add Player2: ");
+        p2 = sc.next();
+        System.out.println("Add Player3: ");
+        p3 = sc.next();
+
+        
+    }
+}
+
